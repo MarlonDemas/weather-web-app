cae13cc8c3010e30b1aa912952f80835c436467c
Marlon <demasmarlon@gmail.com>
2019-03-11 10:38:20 +0200

    Updated server script

--- server.js
+++ server.js
@@ -6,3 +6,10 @@ app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
