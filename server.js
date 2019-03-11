b3d0764315d41275058ec956f4f2a1f1829ac8f2
Marlon <demasmarlon@gmail.com>
2019-03-11 10:58:34 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use('/app/home', oneOfMyRoutes);
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
