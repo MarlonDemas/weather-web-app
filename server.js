507c4b4cccd9c480290d623c307adfb13481dd3d
Marlon <demasmarlon@gmail.com>
2019-03-11 11:01:41 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+const app = express();
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
